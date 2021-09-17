b6b4dec1686cd752fc36a6af77ea2a9b1cf6b9a5
vcoutasso <vcoutasso@gmail.com>
2021-09-17 11:19:49 -0300

    Implementa tela de boas vindas

--- Silent-Moon/ViewController.swift
+++ Silent-Moon/ViewController.swift
@@ -10,14 +10,62 @@ import UIKit
 class ViewController: UIViewController {
     // MARK: - Lazy views
 
-    private lazy var button: RoundedButton = RoundedButton.createBlueButton(title: "SIGN UP")
+    private lazy var signUpButton: RoundedButton = RoundedButton.createBlueButton(title: "SIGN UP")
 
-    private lazy var anotherButton: RoundedButton = {
-        let btn = RoundedButton()
+    private lazy var backgroundIV: UIImageView = {
+        let image = UIImage(named: "BG Tela1")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
-        btn.titleText = "Novo titulo"
-        btn.backgroundColorName = "whiteText"
-        btn.titleColorName = "blue"
+        return imageView
+    }()
+
+    private lazy var ilustraIV: UIImageView = {
+        let image = UIImage(named: "Ilustra_Tela1")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    private lazy var logoIV: UIImageView = {
+        let image = UIImage(named: "Silent Moon Logo")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        return imageView
+    }()
+
+    private lazy var titleStackView: UIStackView = {
+        let title = UILabel()
+        title.text = "We are what we do"
+        title.font = .systemFont(ofSize: 30, weight: .bold)
+        title.textColor = UIColor(named: "black")
+
+        let subtitle = UILabel()
+        subtitle.text = "Thousands of people are using silent moon for small meditations"
+        subtitle.font = .systemFont(ofSize: 16, weight: .light)
+        subtitle.textColor = UIColor(named: "gray")
+        subtitle.numberOfLines = 0
+        subtitle.textAlignment = .center
+
+        let stack = UIStackView(arrangedSubviews: [title, subtitle])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 30
+
+        return stack
+    }()
+
+    private lazy var loginButton: UIButton = {
+        let btn = UIButton()
+
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("ALREADY HAVE AN ACCOUNT? LOG IN", for: .normal)
+        btn.titleLabel?.font = .systemFont(ofSize: 14, weight: .medium)
+        btn.setTitleColor(UIColor(named: "blue"), for: .normal)
+        btn.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
 
         return btn
     }()
@@ -27,34 +75,69 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupViews()
+        setupHierarchy()
+        setupConstraints()
+    }
+
+    // MARK: - Private methods
+
+    private func setupViews() {
         view.backgroundColor = .white
 
         // Adiciona ação de pressionar o botão
-        button.addTarget(self, action: #selector(signinButtonTapped), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+    }
 
+    private func setupHierarchy() {
         // Adiciona botões como subview
-        view.addSubview(button)
-        view.addSubview(anotherButton)
+        view.addSubview(signUpButton)
+        view.addSubview(backgroundIV)
+        view.addSubview(ilustraIV)
+        view.addSubview(logoIV)
+        view.addSubview(titleStackView)
+        view.addSubview(loginButton)
+    }
 
+    private func setupConstraints() {
         // Define constraints pra auto layout
         let constraints = [
-            button.heightAnchor.constraint(equalToConstant: RoundedButton.LayoutMetrics.buttonHeight),
-            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: RoundedButton.LayoutMetrics.buttonHorizontalPadding),
-            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -RoundedButton.LayoutMetrics.buttonHorizontalPadding),
+            backgroundIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            backgroundIV.topAnchor.constraint(equalTo: view.topAnchor),
 
-            anotherButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: RoundedButton.LayoutMetrics.buttonHorizontalPadding),
-            anotherButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -RoundedButton.LayoutMetrics.buttonHorizontalPadding),
-            anotherButton.heightAnchor.constraint(equalToConstant: RoundedButton.LayoutMetrics.buttonHeight),
-            anotherButton.topAnchor.constraint(equalTo: button.bottomAnchor, constant: LayoutMetrics.distanceBetweenButtons)
+            ilustraIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            ilustraIV.bottomAnchor.constraint(equalTo: backgroundIV.topAnchor, constant: LayoutMetrics.ilustraOffset),
+
+            logoIV.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoIV.topAnchor.constraint(equalTo: view.topAnchor, constant: LayoutMetrics.logoTopOffset),
+
+            titleStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleStackView.topAnchor.constraint(equalTo: backgroundIV.bottomAnchor, constant: 15),
+            titleStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: LayoutMetrics.titleHorizontalPadding),
+            titleStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -LayoutMetrics.titleHorizontalPadding),
+
+
+            signUpButton.heightAnchor.constraint(equalToConstant: RoundedButton.LayoutMetrics.buttonHeight),
+            signUpButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: RoundedButton.LayoutMetrics.buttonHorizontalPadding),
+            signUpButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -RoundedButton.LayoutMetrics.buttonHorizontalPadding),
+            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUpButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: LayoutMetrics.signUpButtonBottomOffset),
+
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.topAnchor.constraint(equalTo: signUpButton.bottomAnchor, constant: 20),
         ]
 
         // Ativa as constraints e posiciona na tela
         NSLayoutConstraint.activate(constraints)
     }
 
-    @objc private func signinButtonTapped() {
+    @objc private func signUpButtonTapped() {
+        // FIXME: não é modal, precisamos usar navigation controller
+        present(SignFormViewController(), animated: true, completion: nil)
+        print("usuario fez signup")
+    }
+
+    @objc private func loginButtonTapped() {
         print("usuario fez login")
     }
 
@@ -63,6 +146,10 @@ class ViewController: UIViewController {
     // Ajuda a dar nome e significado para as constantes numéricas, além de facilitar a reutilização e manutenção delas
     private enum LayoutMetrics {
         static let distanceBetweenButtons: CGFloat = 40
+        static let ilustraOffset: CGFloat = 400
+        static let logoTopOffset: CGFloat = 80
+        static let signUpButtonBottomOffset: CGFloat = -128
+        static let titleHorizontalPadding: CGFloat = 58
     }
 }
 
